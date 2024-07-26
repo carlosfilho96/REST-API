d059d5f8d2b722fdb027591e049e639d7adf77a1
Carlos <carlosalmeidafilho96@gmail.com>
2024-07-25 21:50:24 -0300

    250720242150
    Curso de Node JS Aula 06 Criar Rota PUT com parâmetro no Express para atualizar dados

--- src/app.js
+++ src/app.js
@@ -47,4 +47,11 @@ app.delete('/selecoes/:id', (req, res) => {
     res.send(`Seleção exluída com id ${req.params.id} sucesso!`)
 })
 
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
+})
+
 export default app

fb95151aa586aa964e868869462bce8524092060
Carlos <carlosalmeidafilho96@gmail.com>
2024-07-25 21:38:58 -0300

    250720242138
    Curso de Node JS Aula 05 Criar Rotas GET com parâmetro no Express, Criar rota DELETE no Express

--- src/app.js
+++ src/app.js
@@ -9,9 +9,23 @@ app.use(express.json())
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'},
 ]
+
+// retornar o objeto por id
+function buscarSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+// pegar a posicao ou index do elemento no array por id
+function buscarIndexSelecao(id) {
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
 
 // rota padrão ou raiz
 app.get('/', (req, res) => {
@@ -25,6 +39,12 @@ app.get('/selecoes', (req, res) => {
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index,1)
+    res.send(`Seleção exluída com id ${req.params.id} sucesso!`)
 })
 
 export default app

7d6ebe5e2497c44d53a8ca9efdc84b8d07dfc1ec
Carlos <carlosalmeidafilho96@gmail.com>
2024-07-27 00:54:16 -0300

    270720240052
    Curso de Node JS Aula 07 Persistência de Dados com o MySQL, baixar, instalar e conectar

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import app from './src/app.js'
+import conexao from './infra/conexao.js'
 
 const PORT = 3000
 
-// escutar a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-})
+conexao.connect((error => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('Conexão realizada com sucesso!')
+        app.listen(PORT, () =>{
+            console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+        })
+    }
+}))

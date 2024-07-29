d98c75cc690cdcdb68837d73db23a445cec70f5c
Carlos <carlosalmeidafilho96@gmail.com>
2024-07-29 16:22:33 -0300

    290720241622

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 import app from './src/app.js'
-import conexao from './infra/conexao.js'
 
 const PORT = 3000
 
-conexao.connect((error => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log('Conexão realizada com sucesso!')
-        app.listen(PORT, () =>{
-            console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-        })
-    }
-}))
+// escutar a porta 3000
+app.listen(PORT, () =>{
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+})
--- src/app.js
+++ src/app.js
@@ -1,17 +1,10 @@
 import express from 'express'
+import conexao from '../infra/conexao.js'
 
 const app = express()
 
 // indicar apara o express ler body com json
 app.use(express.json())
-
-// mock
-const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Camarões', grupo: 'G'},
-    {id: 4, selecao: 'Sérvia', grupo: 'G'},
-]
 
 // retornar o objeto por id
 function buscarSelecaoPorId(id) {
@@ -23,22 +16,41 @@ function buscarIndexSelecao(id) {
     return selecoes.findIndex(selecao => selecao.id == id)
 }
 
-app.get('/selecoes/:id', (req, res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
-})
-
-// rota padrão ou raiz
-app.get('/', (req, res) => {
-    res.send('Curso de Node JS')
-})
-
+// ROTAS
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes)
+    const sql = "SELECT * FROM selecoes;"
+    conexao.query(sql, (error, result) => {
+        if(error) {
+            res.status(404).json({'erro': error})
+        } else {
+            res.status(200).json(result);
+        }
+    })
+})
+
+app.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM selecoes WHERE id=?;"
+    conexao.query(sql, id, (error, result) => {
+        const row = result[0]
+        if(error) {
+            res.status(404).json({'erro': error})
+        } else {
+            res.status(200).json(row);
+        }
+    })
 })
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
+    const selecao = req.body
+    const sql = "INSERT INTO selecoes SET ?;"
+    conexao.query(sql, selecao, (error, result) => {
+        if(error) {
+            res.status(400).json({'erro': error})
+        } else {
+            res.status(201).json(result);
+        }
+    })    
 })
 
 app.delete('/selecoes/:id', (req, res) => {
